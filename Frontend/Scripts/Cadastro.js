ef6a38a4cc5086262e0d0c364d7239bbb35e70fa
willUlisses <williamcavalcante06@gmail.com>
2026-03-28 12:45:07 -0300

    Hotfix: prevent default do efeito de invalidação do input

--- Frontend/Scripts/Cadastro.js
+++ Frontend/Scripts/Cadastro.js
@@ -34,12 +34,12 @@ for (let opcao of tiposDeCadastro) { // pego cada uma das opções de cadastro (
 todosOsInputs.forEach(input => {
     // ele vai meio que percorrer todos os inputs do formulário e vai verificar se ta invalido, se tiver bota a classe de erro
     input.addEventListener('invalid', function(evento) { 
-        evento.preventDefault()
+        evento.preventDefault();
         evento.target.classList.add('erro-validacao');
     });
 
     input.addEventListener('input', function(evento) { // faz a mesma coisa do primeiro loop mas ele vai tirar o fundo vermelho dos inputs que estão certos.
-        evento.preventDefault()
+        evento.preventDefault();
         evento.target.classList.remove('erro-validacao');
     });
 });
