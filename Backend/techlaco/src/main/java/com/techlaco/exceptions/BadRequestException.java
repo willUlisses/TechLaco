a9db4eb8a4dd91e4c0b7bbad3dea471a27bd02a8
willUlisses <williamcavalcante06@gmail.com>
2026-04-03 18:54:49 -0300

    feat: exception customizada para BadRequest

--- Backend/techlaco/src/main/java/com/techlaco/exceptions/BadRequestException.java
+++ Backend/techlaco/src/main/java/com/techlaco/exceptions/BadRequestException.java
@@ -1,7 +1,7 @@
 package com.techlaco.exceptions;
 
 public class BadRequestException extends RuntimeException {
-  public BadRequestException(String message) {
-    super(message);
-  }
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
