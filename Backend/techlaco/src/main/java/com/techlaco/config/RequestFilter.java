b657d3a60092ac5a925532a1d974975184fff892
willUlisses <williamcavalcante06@gmail.com>
2026-04-03 18:57:41 -0300

    feat: arquivos de configuração dos filtros e do jwt

--- Backend/techlaco/src/main/java/com/techlaco/config/RequestFilter.java
+++ Backend/techlaco/src/main/java/com/techlaco/config/RequestFilter.java
@@ -1,4 +1,57 @@
 package com.techlaco.config;
 
-public class SecurityFilter {
+import com.techlaco.repositories.UsuarioRepository;
+import com.techlaco.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class RequestFilter extends OncePerRequestFilter {
+
+    private final UsuarioRepository usuarioRepository;
+    private final TokenService tokenService;
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = recuperarToken(request);
+
+        if (token != null) {
+            String subject = tokenService.getSubject(token);
+
+            if (subject != null) {
+                usuarioRepository.findById(Long.parseLong(subject)).ifPresent(usuario -> {
+                    var authenticationToken = new UsernamePasswordAuthenticationToken(
+                            usuario,
+                            null,
+                            usuario.getAuthorities()
+                    );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                });
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+    private String recuperarToken(HttpServletRequest request) {
+        String tokenHeader = request.getHeader("Authorization");
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            return tokenHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
 }
--- Backend/techlaco/src/main/java/com/techlaco/config/SecurityConfig.java
+++ Backend/techlaco/src/main/java/com/techlaco/config/SecurityConfig.java
@@ -1,4 +1,53 @@
 package com.techlaco.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final RequestFilter requestFilter;
+
+    //bean dos filtros de segurança dos endpoint
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorized -> authorized
+                        .requestMatchers(HttpMethod.POST, "/auth/cadastro").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll())
+
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
+    }
+    // Bean do administrardor de autenticações
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration)
+            throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    // bean do encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
