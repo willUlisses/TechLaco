5fbec1e8edc47fc15c17e4b747114a2c2fecd54e
willUlisses <williamcavalcante06@gmail.com>
2026-04-03 23:12:53 -0300

    feat: endpoint pra retornar dados do usuário logado atualmente

--- Backend/techlaco/src/main/java/com/techlaco/controllers/AuthController.java
+++ Backend/techlaco/src/main/java/com/techlaco/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.techlaco.controllers;
 
 import com.techlaco.dtos.request.CadastroRequest;
 import com.techlaco.dtos.request.LoginRequest;
+import com.techlaco.dtos.response.AuthResponse;
 import com.techlaco.dtos.response.DadosUsuarioResponse;
 import com.techlaco.entities.Usuario;
 import com.techlaco.services.AuthService;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +26,12 @@ public class AuthController {
     private final TokenService tokenService;
 
     @PostMapping("/cadastro")
-    public ResponseEntity<DadosUsuarioResponse> cadastrarUsuario(@RequestBody CadastroRequest request){
+    public ResponseEntity<AuthResponse> cadastrarUsuario(@RequestBody CadastroRequest request){
         Usuario usuario = authService.cadastrar(request); // cria o usuário e retorna ele
 
         String token = tokenService.gerarToken(usuario); // gera o token com o usuário
 
-         DadosUsuarioResponse response = new DadosUsuarioResponse(
+         AuthResponse response = new AuthResponse(
                 token,
                 usuario.getId(),
                 usuario.getEmail(),
@@ -38,12 +40,25 @@ public class AuthController {
                 usuario.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList()
         );
 
-        return new ResponseEntity<DadosUsuarioResponse>(response, HttpStatus.CREATED);
+        return new ResponseEntity<AuthResponse>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<DadosUsuarioResponse> logarUsuario(@RequestBody LoginRequest request) {
+    public ResponseEntity<AuthResponse> logarUsuario(@RequestBody LoginRequest request) {
         return new ResponseEntity<>(authService.logar(request), HttpStatus.OK);
+    }
+
+    public ResponseEntity<DadosUsuarioResponse> obterUsuarioLogado(@AuthenticationPrincipal Usuario usuario){
+
+        DadosUsuarioResponse response = new DadosUsuarioResponse(
+                usuario.getId(),
+                usuario.getEmail(),
+                usuario.getNome(),
+                usuario.getSobrenome(),
+                usuario.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- Backend/techlaco/src/main/java/com/techlaco/dtos/response/DadosUsuarioResponse.java
+++ Backend/techlaco/src/main/java/com/techlaco/dtos/response/DadosUsuarioResponse.java
@@ -1,15 +1,4 @@
 package com.techlaco.dtos.response;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.List;
-
-public record DadosUsuarioResponse(
-        @NotBlank String token,
-        @NotNull Long id,
-        @NotBlank String email,
-        @NotBlank String nome,
-        @NotBlank String sobrenome,
-        @NotNull List<String> tipo
-) { }
+public record DadosUsuarioResponse() {
+}
