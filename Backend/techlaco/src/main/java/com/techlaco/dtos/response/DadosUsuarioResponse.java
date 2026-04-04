2feab82762a3d17e6aaf29ecb882435c8bcd92f9
willUlisses <williamcavalcante06@gmail.com>
2026-04-03 22:49:55 -0300

    hotfix: mudança na nomenclatura do dto de retorno dos dados do usuário

--- Backend/techlaco/src/main/java/com/techlaco/dtos/response/DadosUsuarioResponse.java
+++ Backend/techlaco/src/main/java/com/techlaco/dtos/response/DadosUsuarioResponse.java
@@ -1,16 +1,15 @@
 package com.techlaco.dtos.response;
 
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record AuthResponse(
+public record DadosUsuarioResponse(
         @NotBlank String token,
         @NotNull Long id,
         @NotBlank String email,
         @NotBlank String nome,
         @NotBlank String sobrenome,
-        @NotNull List<String> tipo)
-{ }
+        @NotNull List<String> tipo
+) { }
