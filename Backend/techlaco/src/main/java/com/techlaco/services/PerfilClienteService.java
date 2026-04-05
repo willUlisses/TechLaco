fd36ec8db787d02495e85fc42ba4618c159ffaef
willUlisses <williamcavalcante06@gmail.com>
2026-04-05 01:56:03 -0300

    feat: criação do método de puxar o perfil do cliente com o total de projetos e método de atualizar a bio

--- Backend/techlaco/src/main/java/com/techlaco/services/PerfilClienteService.java
+++ Backend/techlaco/src/main/java/com/techlaco/services/PerfilClienteService.java
@@ -1,4 +1,39 @@
 package com.techlaco.services;
 
-public class perfilClienteService {
+import com.techlaco.dtos.request.PatchPerfilClienteRequest;
+import com.techlaco.dtos.response.PerfilClienteCompletoResponse;
+import com.techlaco.dtos.response.PerfilClienteResponse;
+import com.techlaco.entities.PerfilCliente;
+import com.techlaco.entities.Usuario;
+import com.techlaco.exceptions.NotFoundException;
+import com.techlaco.repositories.PerfilClienteRepository;
+import com.techlaco.repositories.ProjetosRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PerfilClienteService {
+
+    private final PerfilClienteRepository perfilClienteRepository;
+    private final ProjetosRepository projetosRepository;
+
+    public PerfilClienteCompletoResponse buscarPerfilPorIdUsuario(Usuario usuario) {
+        PerfilCliente perfil = perfilClienteRepository.findByUsuario(usuario).orElseThrow(() -> new NotFoundException("Perfil no encontrado"));
+
+        return PerfilClienteCompletoResponse.from(perfil, projetosRepository.countByPerfilClienteId(perfil.getId()));
+    }
+
+    public PerfilClienteResponse atualizarBio(Usuario usuario, PatchPerfilClienteRequest body) {
+        PerfilCliente perfil = perfilClienteRepository.findByUsuario(usuario).orElseThrow(() -> new NotFoundException("Perfil no encontrado"));
+
+        body.getBio().ifPresent(perfil::setBio);
+
+        perfilClienteRepository.save(perfil);
+        return PerfilClienteResponse.from(perfil);
+    }
+
+
 }
+
+
