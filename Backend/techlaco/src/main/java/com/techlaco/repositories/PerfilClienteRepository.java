b27fe6dad2a54b2114b53e938186c9e48652cb6c
willUlisses <williamcavalcante06@gmail.com>
2026-04-05 01:54:27 -0300

    feat: adição dos métodos necessários pro consumo nos services de projeto e de PerfilCliente

--- Backend/techlaco/src/main/java/com/techlaco/repositories/PerfilClienteRepository.java
+++ Backend/techlaco/src/main/java/com/techlaco/repositories/PerfilClienteRepository.java
@@ -1,9 +1,16 @@
 package com.techlaco.repositories;
 
-import com.techlaco.entities.PerfilFreelancer;
+import com.techlaco.entities.PerfilCliente;
+import com.techlaco.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface PerfilClienteRepository extends JpaRepository<PerfilFreelancer, Long> {
+public interface PerfilClienteRepository extends JpaRepository<PerfilCliente, Long> {
+
+    Optional<PerfilCliente> findByUsuario(Usuario usuario);
+
+
 }
--- Backend/techlaco/src/main/java/com/techlaco/repositories/ProjetosRepository.java
+++ Backend/techlaco/src/main/java/com/techlaco/repositories/ProjetosRepository.java
@@ -1,7 +1,10 @@
 package com.techlaco.repositories;
 
-import com.techlaco.entities.Candidatura;
+import com.techlaco.entities.Projeto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjetosRepository extends JpaRepository<Candidatura, Long> {
+public interface ProjetosRepository extends JpaRepository<Projeto, Long> {
+
+    Long countByPerfilClienteId(Long clienteId);
+
 }
