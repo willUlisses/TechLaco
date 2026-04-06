0a3c0721452138ab76622ab3f761cc9a9480a972
willUlisses <williamcavalcante06@gmail.com>
2026-04-06 00:43:23 -0300

    feat: método para recuperar perfil do freelancer que está logado na aplicação

--- Backend/techlaco/src/main/java/com/techlaco/services/PerfilFreelancerService.java
+++ Backend/techlaco/src/main/java/com/techlaco/services/PerfilFreelancerService.java
@@ -2,8 +2,10 @@ package com.techlaco.services;
 
 import com.techlaco.dtos.request.FiltroBuscarFreelancer;
 import com.techlaco.dtos.response.PageResponse;
-import com.techlaco.dtos.response.PageablePerfilFreelancerResponse;
+import com.techlaco.dtos.response.PerfilFreelancerCompletoResponse;
 import com.techlaco.entities.PerfilFreelancer;
+import com.techlaco.entities.Usuario;
+import com.techlaco.exceptions.NotFoundException;
 import com.techlaco.repositories.PerfilFreelancerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,11 +19,23 @@ public class PerfilFreelancerService {
 
     private final PerfilFreelancerRepository perfilFreelancerRepository;
 
-    public PageResponse<PageablePerfilFreelancerResponse> listarFreelancers(FiltroBuscarFreelancer filtro, Integer pagina, Integer size) {
+    public PageResponse<PerfilFreelancerCompletoResponse> listarFreelancers(FiltroBuscarFreelancer filtro, Integer pagina, Integer size) {
         Pageable pageable = PageRequest.of(pagina, size);
         Page<PerfilFreelancer> paginas = perfilFreelancerRepository.buscarTodosFreelancers(filtro.busca(), pageable);
 
-        return PageResponse.from(paginas.map(PageablePerfilFreelancerResponse::from));
+        return PageResponse.from(paginas.map(PerfilFreelancerCompletoResponse::from));
+    }
+
+    public PerfilFreelancerCompletoResponse buscarPerfilFreelancer(Long id) {
+        PerfilFreelancer perfil = perfilFreelancerRepository.findById(id).orElseThrow(() -> new NotFoundException("Perfil não encontrado"));
+
+        return  PerfilFreelancerCompletoResponse.from(perfil);
+    }
+
+    public PerfilFreelancerCompletoResponse getPerfilFreelancerLogado(Usuario usuario) {
+        PerfilFreelancer perfil = perfilFreelancerRepository.findByUsuario(usuario).orElseThrow(() -> new NotFoundException("Perfil não encontrado a partir do usuário"));
+
+        return PerfilFreelancerCompletoResponse.from(perfil);
     }
 
 }
