98feffe9f5f68fbc2eeebdea50d392f54be6ecb6
willUlisses <williamcavalcante06@gmail.com>
2026-04-06 00:16:06 -0300

    feat: criação do endpoint GET perfis/freelancers

--- Backend/techlaco/src/main/java/com/techlaco/controllers/PerfilFreelancerController.java
+++ Backend/techlaco/src/main/java/com/techlaco/controllers/PerfilFreelancerController.java
@@ -1,4 +1,37 @@
 package com.techlaco.controllers;
 
+import com.techlaco.dtos.request.FiltroBuscarFreelancer;
+import com.techlaco.dtos.response.PageResponse;
+import com.techlaco.dtos.response.PageablePerfilFreelancerResponse;
+import com.techlaco.services.PerfilFreelancerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/perfis/freelancer")
+@RequiredArgsConstructor
 public class PerfilFreelancerController {
+
+    private final PerfilFreelancerService perfilFreelancerService;
+
+    @GetMapping
+    public ResponseEntity<PageResponse<PageablePerfilFreelancerResponse>> buscarFreelancers(
+            @RequestParam(required = false) String busca,
+            @RequestParam(defaultValue = "0") Integer pagina,
+            @RequestParam(defaultValue = "10") Integer limite) {
+
+        Integer limiteSeguro = Math.min(limite, 50);
+
+        FiltroBuscarFreelancer filtro = FiltroBuscarFreelancer.builder()
+                .busca(busca)
+                .build();
+
+        return new ResponseEntity<>(perfilFreelancerService.listarFreelancers(filtro, pagina, limiteSeguro), HttpStatus.OK);
+    }
+
 }
