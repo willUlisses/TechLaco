994758afd03929d447947be6ecb9b9c6b1832f40
willUlisses <williamcavalcante06@gmail.com>
2026-04-06 00:43:45 -0300

    feat: método de buscar perfil freelancer com base no id do usuário

--- Backend/techlaco/src/main/java/com/techlaco/repositories/PerfilFreelancerRepository.java
+++ Backend/techlaco/src/main/java/com/techlaco/repositories/PerfilFreelancerRepository.java
@@ -1,12 +1,15 @@
 package com.techlaco.repositories;
 
 import com.techlaco.entities.PerfilFreelancer;
+import com.techlaco.entities.Usuario;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PerfilFreelancerRepository extends JpaRepository<PerfilFreelancer, Long> {
@@ -29,4 +32,5 @@ public interface PerfilFreelancerRepository extends JpaRepository<PerfilFreelanc
     """)
     Page<PerfilFreelancer> buscarTodosFreelancers(@Param("busca") String busca, Pageable pageable);
 
+    Optional<PerfilFreelancer> findByUsuario(Usuario usuario);
 }
