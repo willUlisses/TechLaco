4a2c32a3400210e8230517e1b701a2a9f26dec52
willUlisses <williamcavalcante06@gmail.com>
2026-04-09 11:46:42 -0300

    feat: configuração do CORS pra integração com o frontend react

--- Backend/techlaco/src/main/java/com/techlaco/config/SecurityConfig.java
+++ Backend/techlaco/src/main/java/com/techlaco/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 
 @Configuration
@@ -22,11 +23,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final RequestFilter requestFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     //bean dos filtros de segurança dos endpoint
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorized -> authorized
